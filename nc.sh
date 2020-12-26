73c724ffcd63ac13f3809837e9d9fa9e1a567308
nickd2048 <76260908+nickd2048@users.noreply.github.com>
2020-12-26 11:51:37 +0100

    Update nc.sh

--- nc.sh
+++ nc.sh
@@ -10,14 +10,22 @@ unclutter &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
 
+# Variables
+BROWSER="/usr/bin/chromium-browser"
+TARGET="http://localhost/"
+
 # Run Chromium and open tabs
-/usr/bin/chromium-browser --window-size=1920,1080 --kiosk --window-position=0,0 --noerrdialogs --disable-session-crashed-bubble --disable-infobars --disable-pinch --no-first-run --disable-features=TranslateUI http://google.com 
+$BROWSER $TARGET --window-size=1920,1080 --kiosk --incognito --window-position=0,0 --noerrdialogs --disable-session-crashed-bubble --disable-infobars --disable-pinch --no-first-run --disable-features=TranslateUI &
 
+# Anti-idle loop
+# #
+while (true)
+  do
+    xdotool keydown ctrl; xdotool keyup ctrl;
+    sleep 15
+done
 
-# Start the kiosk loop. This keystroke changes the Chromium tab
-# To have just anti-idle, use this line instead:
-# xdotool keydown ctrl; xdotool keyup ctrl;
-# Otherwise, the ctrl+Tab is designed to switch tabs in Chrome
+# Switch tab loop
 # #
 # while (true)
 #   do
